28975c9ad713f42c79544a71f4174ca73db56483
Fikri-IF <ihsanfikri00@gmail.com>
2024-11-22 20:21:42 +0700

    refactor : not using gorm

--- configuration/database.go
+++ configuration/database.go
@@ -1,15 +1,13 @@
 package configuration
 
 import (
+	"database/sql"
 	"fmt"
 	"online-store-golang/helper"
 	"time"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
-func NewDatabase(config Config) *gorm.DB {
+func NewDatabase(config Config) *sql.DB {
 	username := config.Get("DB_USERNAME")
 	password := config.Get("DB_PASSWORD")
 	host := config.Get("DB_HOST")
@@ -17,16 +15,13 @@ func NewDatabase(config Config) *gorm.DB {
 	port := config.Get("DB_PORT")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, dbName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := sql.Open("mysql", dsn)
 	helper.PanicIfError(err)
 
-	sql, err := db.DB()
-	helper.PanicIfError(err)
-
-	sql.SetMaxIdleConns(5)
-	sql.SetMaxOpenConns(20)
-	sql.SetConnMaxLifetime(60 * time.Minute)
-	sql.SetConnMaxIdleTime(10 * time.Minute)
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
 
 	return db
 
